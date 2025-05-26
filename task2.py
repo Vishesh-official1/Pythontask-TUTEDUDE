8b3d13c4600b0de84e9f44d477727b15396ef811
Vishesh Python TuteDude <sanon.vishesh@gmail.com>
2025-05-26 17:48:09 +0530

    Updated all files with actual code

---
+++ task2.py
@@ -0,0 +1,5 @@
+firstname= input("Enter your first name: ")
+lastname= input("Enter your last name: ")
+
+fullname = firstname + " " + lastname
+print("hello,", fullname ,"! Welcome to the Python Program")
